b1271a907bc69e0f067805e7d49bf78b490e6820
Emanuel <emanuel@informatykprogramista.pl>
2022-10-29 12:27:31 +0200

    add react-select with first elements

--- src/components/filteringBar/FilteringTab.js
+++ src/components/filteringBar/FilteringTab.js
@@ -1,27 +1,20 @@
 import "../filteringBar/filteringTab.scss";
+import Select from 'react-select';
 
-const FilteringTab = () => {
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const FilteringTab = ({filters, setFilters}) => {
+
   return (
     <div className="col-12 col-md-3">
       <div className="filteringTab p-3">
-        <select
-          className="form-select mb-2"
-          aria-label="Default select example"
-        >
-          <option selected>Select cuisine</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <select
-          className="form-select mb-2"
-          aria-label="Default select example"
-        >
-          <option selected>Select tools</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
+        <Select options={options} placeholder="Select cuisine" className="mb-2" isMulti onChange={(val) => setFilters({...filters, cuisine: val})}/>
+        <Select options={options} placeholder="Select category" className="mb-2" isMulti onChange={(val) => setFilters({...filters, category: val})}/>
+        
         <select
           className="form-select mb-2"
           aria-label="Default select example"
--- src/pages/finder/Finder.js
+++ src/pages/finder/Finder.js
@@ -1,14 +1,25 @@
 import "../finder/finder.scss";
 import FilteringTab from "../../components/filteringBar/FilteringTab";
 import ListOfRecipes from "../../components/listOfRecipes/ListOfRecipes";
+import { useState } from "react";
 
 const Finder = () => {
+
+  const [filters, setFilters] = useState({});
+
+  // const [cuisine, setCuisine] = useState([]);
+  // const [category, setCategory] = useState([]);
+  // const [tools, setTools] = useState([]);
+  // const [ingredients, setIngredients] = useState([]);
+  // const [intolerance, setIntolerance] = useState([]);
+  // const [sort, setSort] = useState('');
+
   return (
     <>
       <div className="container ">
         <h2 className="mt-5 mb-3">Let's explore some recipes!👨🏻‍🍳</h2>
         <div className="row">
-          <FilteringTab />
+          <FilteringTab filters={filters} setFilters={setFilters} />
           <ListOfRecipes />
         </div>
       </div>
